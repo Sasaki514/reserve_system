1691cc4afe231a6171406f6df4c115fc7a7ca566
sasaki514 <neen.dada.sandara21@gmail.com>
2023-12-17 01:20:25 +0900

    Correct LoginFilter

--- src/controllers/customer/CustomerNewServlet.java
+++ src/controllers/customer/CustomerNewServlet.java
@@ -16,8 +16,8 @@ import models.Customer;
  */
 @WebServlet("/customer/new")
 public class CustomerNewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,24 +26,16 @@ public class CustomerNewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	    request.setAttribute("_token", request.getSession().getId());
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        request.setAttribute("_token", request.getSession().getId());
         request.setAttribute("customer", new Customer());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/customer/new.jsp");
         rd.forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    }
 
 }
--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -36,34 +36,42 @@ public class LoginFilter implements Filter {
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String context_path = ((HttpServletRequest)request).getContextPath();
-        String servlet_path = ((HttpServletRequest)request).getServletPath();
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String context_path = ((HttpServletRequest) request).getContextPath();
+        String servlet_path = ((HttpServletRequest) request).getServletPath();
+        String requestURI = context_path + servlet_path;
 
-        if(!servlet_path.matches("/css.*")) {       // CSSフォルダ内は認証処理から除外する
-            HttpSession session = ((HttpServletRequest)request).getSession();
+        if (!servlet_path.matches("/css.*")) { // CSSフォルダ内は認証処理から除外する
+            HttpSession session = ((HttpServletRequest) request).getSession();
 
             // セッションスコープに保存された従業員（ログインユーザ）情報を取得
-            Customer c = (Customer)session.getAttribute("login_customer");
+            Customer c = (Customer) session.getAttribute("login_customer");
 
-            if(!servlet_path.equals("/login")) {        // ログイン画面以外について
+            if (!servlet_path.equals("/login")) { // ログイン画面以外について
+                //会員の新規登録画面への推移はフィルタの適用外
+                if (requestURI.equals(context_path + "/customer/new")) {
+                    chain.doFilter(request, response);
+                    return;
+                }
+
                 // ログアウトしている状態であれば
                 // ログイン画面にリダイレクト
-                if(c == null) {
-                    ((HttpServletResponse)response).sendRedirect(context_path + "/login");
+                if (c == null) {
+                    ((HttpServletResponse) response).sendRedirect(context_path + "/login");
                     return;
                 }
 
                 // 従業員管理の機能は管理者のみが閲覧できるようにする
-                if(servlet_path.matches("/customer.*") && c.getAdmin_flag() == 0) {
-                    ((HttpServletResponse)response).sendRedirect(context_path + "/");
+                if (servlet_path.matches("/customer.*") && c.getAdmin_flag() == 0) {
+                    ((HttpServletResponse) response).sendRedirect(context_path + "/");
                     return;
                 }
-            } else {                                    // ログイン画面について
+            } else { // ログイン画面について
                 // ログインしているのにログイン画面を表示させようとした場合は
                 // システムのトップページにリダイレクト
-                if(c != null) {
-                    ((HttpServletResponse)response).sendRedirect(context_path + "/");
+                if (c != null) {
+                    ((HttpServletResponse) response).sendRedirect(context_path + "/");
                     return;
                 }
             }
